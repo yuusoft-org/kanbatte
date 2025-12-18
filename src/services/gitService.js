abb3fd4eb6554bcebdbc37257c290afb6a3b7c51
Prabesh002 <prabeshpathak002@gmail.com>
2025-12-18 11:41:41 +0545

    create git service

---
+++ src/services/gitService.js
@@ -0,0 +1,48 @@
+import { exec } from "child_process";
+import { promisify } from "util";
+
+const execAsync = promisify(exec);
+
+export const createGitService = () => {
+  const commit = async (payload) => {
+    const { worktreePath, commitMessage, authorName, authorEmail } = payload;
+    await execAsync(`git config user.name "${authorName}"`, {
+      cwd: worktreePath,
+    });
+    await execAsync(`git config user.email "${authorEmail}"`, {
+      cwd: worktreePath,
+    });
+    await execAsync("git add .", { cwd: worktreePath });
+    await execAsync(`git commit -m "${commitMessage}"`, { cwd: worktreePath });
+  };
+
+  const push = async (payload) => {
+    const { worktreePath, branchName } = payload;
+    await execAsync(`git push origin ${branchName} --force`, {
+      cwd: worktreePath,
+    });
+  };
+
+  const createPR = async (payload) => {
+    const { worktreePath, branchName, title, body } = payload;
+    try {
+      await execAsync(`gh pr view ${branchName}`, { cwd: worktreePath });
+      return;
+    } catch (error) {
+      if (!error.stderr.includes("no pull requests found for branch")) {
+        throw error;
+      }
+    }
+
+    await execAsync(
+      `gh pr create --base "main" --head "${branchName}" --title "${title}" --body "${body}"`,
+      { cwd: worktreePath },
+    );
+  };
+
+  return {
+    commit,
+    push,
+    createPR,
+  };
+};
